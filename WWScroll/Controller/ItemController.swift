236907faa129615104e14fcd4f5d413c18777c17
Emmanuel Ramirez <emmanuel.ramirez@weightwatchers.com>
2019-06-07 09:25:20 -0700

    add delegate and improve mvvm

--- WWScroll/Controller/ItemController.swift
+++ WWScroll/Controller/ItemController.swift
@@ -9,9 +9,19 @@
 import UIKit
 import SDWebImage
 
-class ItemController: UITableViewController {
+protocol DataDelegate: class {
+    func doneLoading()
+}
+
+class ItemController: UITableViewController, DataDelegate {
     
-    var itemViewModels = [ItemCellViewModel]()
+    var item = ItemsViewModel()
+    
+    func doneLoading() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 3
@@ -22,7 +32,7 @@ class ItemController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemViewModels.count
+        return item.itemViewModels.count
     }
     
     let headerTitles = ["Section 1", "Section 2", "Section 3"]
@@ -35,19 +45,19 @@ class ItemController: UITableViewController {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as? ItemCell {
             switch indexPath.section {
             case 1:
-                cell.foodTitle.text = "\(itemViewModels[indexPath.row].title)"
+                cell.foodTitle.text = "\(item.itemViewModels[indexPath.row].title)"
                 cell.foodImageView.image = nil
             case 2:
                 cell.foodTitle.text = nil
-                let imageURL =  itemViewModels[indexPath.row].imageName
+                let imageURL =  item.itemViewModels[indexPath.row].imageName
                 let domain = URL(string: "https://www.weightwatchers.com/\(imageURL)")
 //                cell.foodImageView.image = UIImage(named: "bear_first")
                 cell.foodImageView.sd_setImage(with: domain)
             default:
-                cell.foodTitle.text = "\(itemViewModels[indexPath.row].title)"
-                let imageURL =  itemViewModels[indexPath.row].imageName
+                cell.foodTitle.text = "\(item.itemViewModels[indexPath.row].title)"
+                let imageURL =  item.itemViewModels[indexPath.row].imageName
                 let domain = URL(string: "https://www.weightwatchers.com/\(imageURL)")
-                cell.foodImageView.sd_setImage(with: domain, placeholderImage: UIImage(named: "bear_first"), options: [], completed: nil)
+                cell.foodImageView.sd_setImage(with: domain)//, placeholderImage: UIImage(named: "bear_first"), options: [], completed: nil)
             }
             return cell
         } else  {
@@ -56,51 +66,13 @@ class ItemController: UITableViewController {
         
     }
     
-    
-    private func loadData() {
-        let sharedSession = URLSession.shared
-        if let url = URL(string: "https://www.weightwatchers.com/assets/cmx/us/messages/collections.json") {
-            let request = URLRequest(url: url)
-            let dataTask = sharedSession.dataTask(with: request, completionHandler: { (data, response, error) in
-                if error != nil {
-                    return
-                }
-                if let httpStatus = response as? HTTPURLResponse {
-                    if httpStatus.statusCode != 200 {
-                        return
-                    }
-                }
-                do {
-                    //look good??
-                    if let theData = data, let dataDictionary = try JSONSerialization.jsonObject(with: theData, options: .allowFragments) as? [NSDictionary] {
-                        for data in dataDictionary {
-                            //                        let imageName = data["image"].unsafelyUnwrapped
-                            //                        let title = data["title"].unsafelyUnwrapped
-                            if let imageName = data["image"] as? String, let title = data["title"] as? String { //look good?
-                                let item = Item(imageName: imageName, title: title)
-                                self.itemViewModels.append(ItemCellViewModel(item: item))
-                            }
-                            
-                        }
-                        DispatchQueue.main.async {
-                            self.tableView.reloadData()
-                        }
-                    } else {
-                        return
-                    }
-                } catch {
-                    return
-                }
-            })
-            dataTask.resume()
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(ItemCell.self, forCellReuseIdentifier: "cellId")
         tableView.dataSource = self
         tableView.delegate = self
-        loadData()
+        item.delegate = self
+        item.loadData() //tableView: tableView)
+        
     }
 }
--- WWScroll/ViewModel/ItemsViewModel.swift
+++ WWScroll/ViewModel/ItemsViewModel.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2019 Emmanuel Ramirez. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+
+class ItemsViewModel: NSObject {
+    
+    weak var delegate: DataDelegate?
+
+    var itemViewModels = [ItemCellViewModel]()
+    
+    func loadData(){//tableView: UITableView) {
+        let sharedSession = URLSession.shared
+        if let url = URL(string: "https://www.weightwatchers.com/assets/cmx/us/messages/collections.json") {
+            let request = URLRequest(url: url)
+            let dataTask = sharedSession.dataTask(with: request, completionHandler: { (data, response, error) in
+                if error != nil {
+                    return
+                }
+                if let httpStatus = response as? HTTPURLResponse {
+                    if httpStatus.statusCode != 200 {
+                        return
+                    }
+                }
+                do {
+                    //look good??
+                    if let theData = data, let dataDictionary = try JSONSerialization.jsonObject(with: theData, options: .allowFragments) as? [NSDictionary] {
+                        for data in dataDictionary {
+                            if let imageName = data["image"] as? String, let title = data["title"] as? String { //look good?
+                                let item = Item(imageName: imageName, title: title)
+                                self.itemViewModels.append(ItemCellViewModel(item: item))
+                            }
+                        }
+                        self.delegate?.doneLoading()
+                    } else {
+                        return
+                    }
+                } catch {
+                    return
+                }
+            })
+            dataTask.resume()
+        }
+    }
+}
+
+
